b3ed8762e7ab1d3757651eaf3dfa7cd4f117ef49
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2021-12-19 16:24:02 +0100

    Adding Idex and Controller
    Closes #8

--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class PetClinicApplicationTests
+public class PetClinicApplicationTests
 {
 	@Test
-	void contextLoads()
+	public void contextLoads()
 	{
 	}
 }
