3eae316f732d18ee3af3780bcb081ba2cc8f5734
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2022-01-08 17:04:21 +0100

    Added Owners List
    Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnersController.java
@@ -1,15 +1,24 @@
 package guru.springframework.petclinic.controllers;
 
+import guru.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController
 {
+	private final OwnerService	ownerService;
+
+	public OwnersController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String	index()
+	public String	index(Model model)
 	{
+		model.addAttribute("owners", ownerService.findAll());
 		return ("owners/index");
 	}
 }
