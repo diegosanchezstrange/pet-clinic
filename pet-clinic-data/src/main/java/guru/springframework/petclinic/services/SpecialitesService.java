b831581d429fe5141c6e6cef3be2c5225ce505b1
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2022-01-12 15:44:12 +0100

    Added Specialties Map Impl
    closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Specialty;
+
+public interface SpecialitesService extends CrudService<Specialty, Long>{
+}
